85d1138269e82e5c5e9d9f47071da70add60defe
Rogerio-Lacerda <70972583+Rogerio-Lacerda@users.noreply.github.com>
2023-02-09 18:05:13 -0300

    Outra correção de bug

--- js/vendas.js
+++ js/vendas.js
@@ -29,3 +29,33 @@ $(function () {
 
 
 });
+
+$(function () {
+  $(".watch-slider2").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true,
+    prevArrow: $("#arrow-prev2"),
+    nextArrow: $("#arrow-next2"),
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 840,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          // centerMode: false,
+        },
+      },
+      {
+        breakpoint: 670,
+        settings: {
+          centerMode: false,
+        },
+      },
+    ],
+  });
+});
